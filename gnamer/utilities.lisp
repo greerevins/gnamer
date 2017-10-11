8df6c4c71176e6fd0ccb0ae019d81cb2f9810d53
greer evins <gmlouise.evins@gmail.com>
2017-10-10 20:04:12 -0600

    we added readme and code to find the gnamer directory

--- gnamer/utilities.lisp
+++ gnamer/utilities.lisp
@@ -4,3 +4,7 @@
 (defun any (a-sequence)
   (elt a-sequence
        (random (length a-sequence))))
+
+(defun find-gnamer-home-pathname ()
+  (merge-pathnames "gnamer/" 
+                   (user-homedir-pathname)))
