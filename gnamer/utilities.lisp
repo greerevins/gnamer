964094280d55e8ddc810eca9d66a915fac66344f
mikel evins <mevins@me.com>
2018-03-20 17:26:20 -0500

    added LINES and CHARACTERS functions

--- gnamer/utilities.lisp
+++ gnamer/utilities.lisp
@@ -8,5 +8,33 @@
   (elt seq
        (random (length seq))))
 
+;;; GENERIC FUNCTION lines (path)
+;;; ---------------------------------------------------------------------
+;;; reads the contents of the file at PATH line-by-line, returning
+;;; a list of the resulting lines
 
+(defmethod lines ((path pathname))
+  (with-open-file (in path)
+    (loop for line = (read-line in nil nil nil)
+       then (read-line in nil nil nil)
+       while line
+       collect line)))
+
+(defmethod lines ((path string))
+  (lines (pathname path)))
+
+;;; GENERIC FUNCTION characters (path)
+;;; ---------------------------------------------------------------------
+;;; copies the contents of the file at PATH into a string and returns
+;;; the string
+
+(defmethod characters ((path pathname))
+  (with-open-file (in path)
+    (let* ((char-count (file-length in))
+           (buffer (make-array char-count :element-type 'character :initial-element #\null)))
+      (read-sequence buffer in)
+      buffer)))
+
+(defmethod characters ((path string))
+  (characters (pathname path)))
 
