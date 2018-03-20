980f7453da71d2250cd6b597b71eb9a3b777ba03
mikel evins <mevins@me.com>
2018-03-20 17:16:02 -0500

    added a comment describing ANY

--- gnamer/utilities.lisp
+++ gnamer/utilities.lisp
@@ -1,7 +1,12 @@
 (in-package :namer)
 
+;;; GENERIC FUNCTION any ((seq sequence))
+;;; ---------------------------------------------------------------------
+;;; returns a randomly-chosen element of SEQ
+
 (defmethod any ((seq sequence))
   (elt seq
        (random (length seq))))
+
 
 
